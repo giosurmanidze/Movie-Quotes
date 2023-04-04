42500f2ab8aa88d2bea77628598fb56b6290ff20
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-04-05 01:42:27 +0400

    wip/login-logout

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\SessionsController;
 use App\Http\Controllers\QuoteController;
 use Illuminate\Support\Facades\Route;
@@ -8,9 +9,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [QuoteController::class, 'index'])->name('quotes.index');
 Route::get('movies/{id}/quotes', [QuoteController::class, 'show'])->name('quotes.show');
 
-Route::get('admin/login', [SessionsController::class, 'create'])->middleware('guest');
+Route::get('admin/login', [SessionsController::class, 'create'])->middleware('guest')->name('login');
 Route::post('admin/login', [SessionsController::class, 'store'])->middleware('guest');
+Route::post('logout', [SessionsController::class, 'distroy'])->middleware('auth');
 
-Route::get('admin/dashboard', function () {
-    return view('login.dashboard');
-})->middleware(['auth'])->name('login.dashboard');
+Route::get('admin/dashboard', [DashboardController::class, 'create'])->middleware(['auth', 'admin']);
