b40629ecb3ae3febcaed512148aef156f9299c8c
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-04-04 19:40:44 +0400

    fix/quote controller

--- app/Models/Quote.php
+++ app/Models/Quote.php
@@ -7,7 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Quote extends Model
 {
-   
+    use HasFactory;
+
     protected $fillable = ['movie_id', 'quote', 'id'];
 
     public function movie()
--- routes/web.php
+++ routes/web.php
@@ -8,5 +8,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [QuoteController::class, 'index'])->name('quotes.index');
 Route::get('/movies/{id}/quotes', [QuoteController::class, 'show'])->name('quotes.show');
 
-Route::get('admin/login', [SessionsController::class, 'create']);
-Route::post('admin/login', [SessionsController::class, 'store']);
+Route::get('admin/login', [SessionsController::class, 'create'])->middleware('guest');
+Route::post('admin/login', [SessionsController::class, 'store'])->middleware('guest');
