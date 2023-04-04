6d0cf0adef41f41f56e002469a02949e66aa0c20
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-04-05 00:37:39 +0400

    fix/login feature issue

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,11 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', [QuoteController::class, 'index'])->name('quotes.index');
-Route::get('/movies/{id}/quotes', [QuoteController::class, 'show'])->name('quotes.show');
+Route::get('movies/{id}/quotes', [QuoteController::class, 'show'])->name('quotes.show');
 
 Route::get('admin/login', [SessionsController::class, 'create'])->middleware('guest');
 Route::post('admin/login', [SessionsController::class, 'store'])->middleware('guest');
+
+Route::get('admin/dashboard', function () {
+    return view('login.dashboard');
+})->middleware(['auth'])->name('login.dashboard');
