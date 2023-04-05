e8857354cd47a07dbeb96b4e98e87eda1b5bf59e
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-04-05 21:14:54 +0400

    style/style dashboard screen

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -31,7 +31,7 @@ class SessionsController extends Controller
         }
         session()->regenerate();
 
-        return redirect('/admin/dashboard');
+        return redirect('admin/dashboard/create-quote');
     }
 
     public function logout()
