0a524d2ebd93a14664bf521e15dbc48c7f2d2183
giosurmanidze <giorgisurmanidze917@gmail.com>
2023-04-05 22:10:19 +0400

    feat/create movie

--- resources/views/login/dashboard.blade.php
+++ resources/views/login/dashboard.blade.php
@@ -1,21 +1,4 @@
-<div class="flex text-white items-center w-full mt-3 justify-around">
-    <div class="flex gap-3">
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium {{ request()->is('admin/dashboard/') ? 'bg-gray-900 text-white' : '' }}">Edit Quote</a>
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium {{ request()->is('edit-movie') ? 'bg-gray-900 text-white' : '' }}">Edit Movie</a>
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium {{ request()->is('admin/dashboard/create-quote') ? 'bg-gray-900 text-white' : '' }}">Create Quote</a>
-        <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium {{ request()->is('create-movie') ? 'bg-gray-900 text-white' : '' }}">Create Movie</a>
-    </div>
-    
-    <div class="flex items-center">
-      <h1 class="text-xl font-semibold">Welcome Back, {{ auth()->user()->name }}</h1>
-      <form method="POST" action='/logout' class="text-sm font-semibold text-blue-500 ml-6">
-        @csrf
-        <button type="submit" class="text-md hover:bg-blue-500 hover:text-white px-3 py-2 rounded-md">Log out</button>
-      </form>
-    </div>
-  </div>
-  
-
+<x-navbar />
 <x-layout>
     <x-slot name="content">
         <form method="POST" action="/admin/quotes" enctype="multipart/form-data" class="mt-8">
@@ -39,7 +22,8 @@
                     image
                 </label>
 
-                <input class="border border-gray-400 p-2 w-full rounded-md" type="file" name="image" id="image" required>
+                <input class="border border-gray-400 p-2 w-full rounded-md" type="file" name="image" id="image"
+                    required>
 
                 @error('image')
                     <p class="text-red-500 text-xs mt-2">{{ $message }}</p>
@@ -67,7 +51,5 @@
                 </button>
             </div>
         </form>
-
-
     </x-slot>
 </x-layout>
