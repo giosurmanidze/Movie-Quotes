2bcaad066f743361d680b141d07973b9d6029439
giosurmanidze <giorgi.surmanidze@redberry.ge>
2023-04-07 15:31:17 +0400

    feat/add static translate

--- resources/views/quotes/index.blade.php
+++ resources/views/quotes/index.blade.php
@@ -2,7 +2,7 @@
     <x-slot name="content">
         @auth
             <a href="/admin/dashboard/create-quote"
-                class="m-5 inline-block px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-600">Dashboard</a>
+                class="m-5 inline-block px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-600">{{__("Dashboard")}}</a>
         @endauth
         <div class="text-white flex flex-col items-center max-w-2xl w-[650px] top-1/2 transform translate-y-1/3">
             <x-image-check class="w-full h-[350px] rounded-xl" :quote="$quote" />
@@ -10,5 +10,6 @@
             <a class="mt-2 underline text-xl font-Noto"
                 href="{{ route('quotes.show', $quote->id) }}">{{ $quote->movie?->title }}</a>
         </div>
+        <x-lang-control  path_name='quotes.index' />
     </x-slot>
 </x-layout>
